78c0a4f33b8bf9c742162ddfd48fe20f1485b631
Christina <christinaraganit@gmail.com>
2022-09-18 22:35:54 -0700

    push push :pushpin:

--- coffee-cake/pages/index.js
+++ coffee-cake/pages/index.js
@@ -4,18 +4,23 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="home">
+    <div id="home" className="home h-screen w-screen">
       <Head>
         <title>Coffee Cake</title>
         <meta
           name="description"
           content="Goldentoaste's default portfolio website"
         />
+        <script src="https://cdn.tailwindcss.com"></script>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <h1 className="text-8xl">Press any key to start</h1>
+      <div className="flex flex-col h-screen content-center justify-center items-center align-center">
+        <img className="h-40 mb-24" src="./rat-gong.svg"></img>
+
+        <h1 className="text-2xl tracking-widest text-chamois-200 animate-pulse">
+          Press any key to start
+        </h1>
       </div>
     </div>
   );
--- coffee-cake/tailwind.config.js
+++ coffee-cake/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./pages/index.js"],
   theme: {
+    colors: {
+      chamois: {
+        100: "#F7F1E0",
+        200: "#F5EDD8",
+        300: "#F3E9D1",
+        400: "#F1E6C9",
+        500: "#EFE2C1",
+        600: "#EDDFBA",
+        700: "#EBDBB2",
+        800: "#D6C8A3",
+        900: "#BCAF8E",
+      },
+    },
     extend: {
+      backgroundImage: {
+        background: "url('/public/background.png')",
+      },
       fontFamily: {
         rodin: ["Rodin", "sans-serif"],
       },
